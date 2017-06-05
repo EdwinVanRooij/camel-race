961b9ba83f2e3aba5cde92e194c5257dcd457365
Edwin van Rooij <edwinvrooij@gmail.com>
2017-06-05 11:39:37 +0200

    moved stuff to emulator

---
+++ src/main/io/github/edwinvanrooij/ClientEmulator.java
@@ -0,0 +1,81 @@
+package io.github.edwinvanrooij;
+
+/**
+ * Created by eddy
+ * on 6/5/17.
+ */
+public class ClientEmulator implements Runnable {
+    @Override
+    public void run() {
+        CamelRaceEngine engine = new CamelRaceEngine();
+
+        printMap(engine.generateGameState());
+
+        try {
+            for (int i = 0; i < 100; i++) {
+                engine.nextRound();
+                printNewRound(i);
+                GameState gameState = engine.generateGameState();
+                if (gameState.isGameEnded()) {
+                    printWinner(gameState.getWinner());
+                    printMap(gameState);
+                    break;
+                } else {
+                    printMap(gameState);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void printWinner(Camel camel) {
+        System.out.println("===================================");
+        System.out.println(String.format("============ We have a winner! Camel %s! ============", camel));
+        System.out.println("===================================");
+    }
+
+    private static void printNewRound(int round) {
+        System.out.println();
+        System.out.println();
+        System.out.println("===================================");
+        System.out.println(String.format("============ ROUND %s  ============", ++round));
+        System.out.println("===================================");
+        System.out.println();
+        System.out.println();
+    }
+
+    private static void printMap(GameState gameState) {
+        printSpace();
+
+        for (SideCard c : gameState.getSideCardList()) {
+            System.out.println(c);
+        }
+
+        printSpace();
+
+        for (Camel c : gameState.getCamelList()) {
+            System.out.println(c);
+        }
+
+        printSpace();
+
+//        for (Card c : gameState.getDeck()) {
+//            System.out.println(c);
+//        }
+
+        printSpace();
+
+        if (gameState.getLastPickedCard() != null) {
+            System.out.format("Last picked card: %s\n", gameState.getLastPickedCard());
+        } else {
+            System.out.println("Game just started, no last picked card yet.");
+        }
+    }
+
+    private static void printSpace() {
+        for (int i = 0; i < 1; i++) {
+            System.out.println();
+        }
+    }
+}
